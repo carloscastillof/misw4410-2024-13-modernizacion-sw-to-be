34d3eb6f00456771e510ee9d82b25fb8561457bd
Jesus Henriquez <henriquezfilardo@icloud.com>
2024-07-28 15:58:54 -0500

    fix: update size entity

--- msv-products/src/domain/entities/Size.ts
+++ msv-products/src/domain/entities/Size.ts
@@ -1,29 +1,32 @@
-import { Entity, PrimaryColumn, Column, OneToOne, JoinColumn } from "typeorm";
+import { Entity, PrimaryColumn, Column, OneToOne, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 import { Product } from "./Product";
 
 @Entity()
 export class Size {
-  @PrimaryColumn()
-  product_id: number | undefined;
+    @PrimaryGeneratedColumn()
+    id: number | undefined;
+    
+    @Column({ type: "int", nullable: true })
+    product_id: number | undefined;
 
-  @OneToOne(() => Product)
-  @JoinColumn({ name: "product_id" })
-  product: Product;
+    @ManyToOne(() => Product, product => product.id)
+    @JoinColumn({ name: "product_id" })
+    product: Product;
 
-  @Column()
-  size: string;
+    @Column()
+    size: string;
 
-  @Column("decimal", { precision: 10, scale: 2 })
-  price: number;
+    @Column("decimal", { precision: 10, scale: 2 })
+    price: number;
 
-  @Column()
-  stock: number;
+    @Column()
+    stock: number;
 
-  constructor(product: Product, size: string, price: number, stock: number) {
-    this.product_id = product !== undefined ? product.id : 0;
-    this.product = product;
-    this.size = size;
-    this.price = price;
-    this.stock = stock;
-  }
+    constructor(product: Product, size: string, price: number, stock: number) {
+        this.product_id = product !== undefined ? product.id : 0;
+        this.product = product;
+        this.size = size;
+        this.price = price;
+        this.stock = stock;
+    }
 }
